ebef2a01a54ac5aba95d26e8c631fc0df6f8dd12
Lotus Biswas <131962991+l0tus-biswas@users.noreply.github.com>
2023-12-13 16:11:51 +0530

    add all files

---
+++ test.js
@@ -0,0 +1,21 @@
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: '173.212.225.166',
+    port: 3306, // Default MySQL port
+  user: 'root',
+  password: 'SribasBis@22',
+  database: 'neuzmail_ori',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+pool.query('SELECT 1 + 1', (err, results) => {
+  if (err) {
+    console.error('Error executing query:', err);
+    return;
+  }
+  console.log('Connected to MySQL server:', results);
+});
