4b454a6ac8b51026aebec762c71b2e67432d2701
Lotus Biswas <131962991+l0tus-biswas@users.noreply.github.com>
2023-12-16 22:26:40 +0530

    email to dealid for 3rd

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-
+const express = require('express');
 const mysql = require('mysql2');
+
+const app = express();
 
 const pool = mysql.createPool({
   host: '173.212.225.166',
-    port: 3306, // Default MySQL port
+  port: 3306,
   user: 'root',
   password: 'SribasBis@22',
   database: 'neuzmail_ori',
@@ -12,10 +14,26 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
-pool.query('SELECT 1 + 1', (err, results) => {
-  if (err) {
-    console.error('Error executing query:', err);
-    return;
-  }
-  console.log('Connected to MySQL server:', results);
+// Endpoint to retrieve data from the 'users' table
+app.get('/users', (req, res) => {
+  // Query to select all rows from the 'users' table
+  const sql = 'SELECT * FROM users';
+
+  // Execute the query
+  pool.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    // Send the results as JSON
+    res.json(results);
+  });
+});
+
+// Start the Express server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
